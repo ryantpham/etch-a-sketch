c811cbc3d00638e5294c38d377a7b8247f2cf25c
Ryan Pham <rtpham10@gmail.com>
2024-03-02 21:41:45 -0800

    Added Reset button feature

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const sketchPad = document.querySelector('.container')
+const resetButton = document.querySelector('.reset')
+
 //Creates 16x16 Grid
 for(i=0;i<256;i++){
+//Creates Grid and Draw
 const square = document.createElement('div')
 square.classList.add('square');
 sketchPad.appendChild(square);
+
+square.addEventListener('mouseover',()=>{
+    square.style.backgroundColor = "Gray";
+})
+
+//Reset Button
+resetButton.addEventListener('click', ()=>{
+    square.style.backgroundColor = "White";
+})
 }
+
+
+
